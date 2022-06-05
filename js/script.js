4b30bfc6c5ef63c48298ddc2630a274e8767c0c7
SathyanAiswarya <aiswaryasathyan2012@gmail.com>
2022-06-05 22:35:24 +0200

    create a basic To-Do-List app.
    1.The user can add a new item to a list of items.
    2.The user can cross out an item from the list of items.
    3.The user can delete an item from the list of items.
    4.The user can change the order of items in the list of items.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// for adding items to the list
+function newItem() {
+    let li = $('<li></li>');
+    let textValue = $('#input').val();
+    li.append(document.createTextNode(textValue));
+
+
+    if (textValue == '') {
+        alert('This field is required');
+    }
+    else {
+        $('#list').append(li);
+        console.log(textValue);
+    }
+
+
+    //deleting the item by clicking on cross and striking the item in double click. 
+        let crossOutButton = $('<crossOutButton></crossOutButton>');
+        crossOutButton.append(document.createTextNode('X'));
+        li.append(crossOutButton);
+    
+
+    crossOutButton.on('click', function () {
+        li.addClass('delete');
+    });
+
+    li.on('dblclick', function () {
+        li.toggleClass('strike');
+    });
+
+    // rearranging the list
+    $('#list').sortable();
+
+
+}
